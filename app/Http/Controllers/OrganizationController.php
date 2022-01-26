a1970dfc78a7d9823d7a61019081667361fd46d8
Fernando Daniel Romano <fernandoreceptor2014@gmail.com>
2022-01-25 23:51:27 -0300

    crud of organizations ready

---
+++ app/Http/Controllers/OrganizationController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\OrganizationRequest;
+use App\Models\Organization;
+
+class OrganizationController extends Controller
+{
+    
+    public function index(){
+        return response()->json([
+            "data" => Organization::with('user')->get()
+        ], 200);
+    }
+
+    public function store(OrganizationRequest $request){
+        $organization = Organization::create([
+            "name" => $request->name,
+            "user_id" => auth()->user()->id
+        ]);
+
+        return response()->json([
+            "message" => "Organization created!!",
+            "data" => $organization
+        ], 201);
+    }
+
+    public function show(Organization $organization){
+        return response()->json([
+            "data" => $organization
+        ], 200);
+    }
+
+    public function update(OrganizationRequest $request, Organization $organization){
+        $organization->name = $request->name;
+        $organization->save();
+
+        return response()->json([
+            "message" => "Organization updated!!",
+            "data" => $organization,
+        ], 200);
+    }
+
+    public function destroy(Organization $organization){
+        $organization->delete();
+
+        return response()->json([
+            "message" => "Organization delete!!",
+        ], 200);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OrganizationController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,16 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::post('register', [UserController::class, 'register']);
-Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register'])->name('register');
+Route::post('login', [UserController::class, 'login'])->name('login');
 
 Route::middleware(['auth:sanctum'])->group(function(){
 
-    Route::get('user-profile', [UserController::class, 'profile']);
-    Route::post('logout', [UserController::class, 'logout']);
+    Route::get('user-profile', [UserController::class, 'profile'])->name('profile');
+    Route::post('logout', [UserController::class, 'logout'])->name('logout');
+
+    Route::apiResource('organizations', OrganizationController::class);
 
 });
