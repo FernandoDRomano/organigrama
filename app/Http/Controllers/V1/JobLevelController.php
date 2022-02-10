cbe0987efe4534ee5d4bed2239cbc9beb4509e20
Fernando Daniel Romano <fernandoreceptor2014@gmail.com>
2022-02-10 18:28:26 -0300

    jobLevelResource ready

--- app/Http/Controllers/V1/JobLevelController.php
+++ app/Http/Controllers/V1/JobLevelController.php
@@ -4,39 +4,44 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\JobLevelRequest;
+use App\Http\Resources\JobLevelResource;
 use App\Models\JobLevel;
 
 class JobLevelController extends Controller
 {
     
     public function index(){
-        return response()->json([
-            "data" => JobLevel::all()
-        ], 200);
+        $jobLevels = JobLevel::paginate(10);
+
+        return (JobLevelResource::collection($jobLevels))
+               ->additional(["message" => "Job levels all!!"])
+               ->response()
+               ->setStatusCode(200);
     }
 
     public function store(JobLevelRequest $request){
         $jobLevel = JobLevel::create($request->all());
 
-        return response()->json([
-            "message" => "Job level created!!",
-            "data" => $jobLevel
-        ], 201);
+        return (JobLevelResource::make($jobLevel))
+               ->additional(["message" => "Job level created!!"])
+               ->response()
+               ->setStatusCode(201);
     }
 
     public function show(JobLevel $jobLevel){
-        return response()->json([
-            "data" => $jobLevel
-        ], 200);
+        return (JobLevelResource::make($jobLevel))
+               ->additional(["message" => "Job level!!"])
+               ->response()
+               ->setStatusCode(200);
     }
 
     public function update(JobLevel $jobLevel, JobLevelRequest $request){
         $jobLevel->fill($request->all())->save();
 
-        return response()->json([
-            "message" => "Job level updated!!",
-            "data" => $jobLevel
-        ], 200);
+        return (JobLevelResource::make($jobLevel))
+               ->additional(["message" => "Job level updated!!"])
+               ->response()
+               ->setStatusCode(200);
     }
 
     public function destroy(JobLevel $jobLevel){
@@ -44,7 +49,7 @@ class JobLevelController extends Controller
 
         return response()->json([
             "message" => "Job level deleted!!"
-        ], 200);
+        ], 204);
     }
 
 }
